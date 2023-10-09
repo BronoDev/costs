50a0ad8dd560d87021e790f316fc25acb5521be5
Brono <brunobrunelli1995@gmail.com>
2023-10-09 10:22:13 -0300

    ✨ feat: Criando componente de loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 
 import Message from "../layouts/Message"
 import Container from "../layouts/Container"
+import Loading from "../layouts/Loading"
 import LinkButton from "../layouts/LinkButton"
 import ProjectCard from "../project/ProjectCard"
 
@@ -12,6 +13,7 @@ import styles from "./Projects.module.css"
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -20,16 +22,19 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then((resp) => resp.json())
-            .then((data) => {
-                setProjects(data)
-            })
-            .catch((err) => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((resp) => resp.json())
+                .then((data) => {
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 3000);
     }, [])
 
     return (
@@ -50,6 +55,10 @@ function Projects() {
                             key={project.id}
                         />
                     ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
