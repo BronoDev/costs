7fe72c2aee9689f4d0d02932c66325042980980e
Brono <brunobrunelli1995@gmail.com>
2023-10-09 10:40:44 -0300

    ✨ feat: Removendo projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,6 +14,7 @@ import styles from "./Projects.module.css"
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -37,13 +38,29 @@ function Projects() {
         }, 3000);
     }, [])
 
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+            .then(data => {
+                setProjects(projects.filter((project => project.id !== id)))
+                setProjectMessage('Projeto removido com sucesso!')
+            })
+            .catch(err => console.log(err))
+
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
-            {message && <Message type="sucess" msg={message} />}
+            {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -53,6 +70,7 @@ function Projects() {
                             budget={project.budget}
                             category={project?.category?.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />
                     ))}
                 {!removeLoading && <Loading />}
